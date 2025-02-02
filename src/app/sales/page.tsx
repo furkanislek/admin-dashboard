f989e0321b49bb759b8a766cf5a8de39beb8741f
furkanislek <furkanakifislek@gmail.com>
2025-02-02 20:59:25 +0300

    fixs

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -22,7 +22,7 @@ const Sales = () => {
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Sales Dashboard" />
 
-      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+      <main className="max-w-full mx-auto py-6 px-4 lg:px-8">
         {/* SALES STATS */}
         <motion.div
           className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -10,12 +10,14 @@ const Settings = () => {
   	return (
       <div className="flex-1 overflow-auto relative z-10 bg-gray-900">
         <Header title="Settings" />
-        <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8">
+        <main className="max-w-full  mx-auto py-6 px-4 lg:px-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <Profile />
           <Notifications />
           <Security />
           <ConnectedAccounts />
           <DangerZone />
+          </div>
         </main>
       </div>
     );
