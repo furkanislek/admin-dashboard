8d5be3f9173ee02de022d8cb65d5ffee08769978
furkanislek <furkanakifislek@gmail.com>
2025-02-02 20:46:08 +0300

    title

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 const Header = ({ title }: HeaderProps) => {
   return (
     <header className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700">
-      <div className="max-w-full min-w-full py-4 px-4 sm:px-6 lg:px-8 " >
+      <div className="max-w-full min-w-full py-4 px-4 sm:px-6 lg:px-8" >
         <h1 className="text-2xl font-semibold text-gray-100">{title}</h1>
       </div>
     </header>
