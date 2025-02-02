dc2fc65da5811e6b43d16af7509e4900d6731739
furkanislek <furkanakifislek@gmail.com>
2025-02-02 21:07:28 +0300

    fixs

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -21,7 +21,7 @@ const Analytics = () => {
     <div className="flex-1 overflow-auto relative z-10 bg-gray-900">
       <Header title={"Analytics Dashboard"} />
 
-      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+      <main className="max-w-full mx-auto py-6 px-4 lg:px-8">
         <OverviewCards />
         <RevenueChart />
 
